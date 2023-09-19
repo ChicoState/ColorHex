be8ecf6894c7c7f891aa4fc16314a0ffe1ca4ca4
numan-7 <numankhan165@gmail.com>
2023-09-19 12:37:03 -0700

    Added character checks, fixed #1

--- main.cpp
+++ main.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 const int RGB_HEX_LENGTH = 7;
 
 int main(){
     string input;
-
+    bool error = true;
+    bool syntaxErr = false;
     do{
         cout << "Enter a color in hex format (#RRGGBB):";
         getline(cin, input);
-
-        if( input.size() != RGB_HEX_LENGTH ){
-            cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+        for(int i = 1; i < input.size(); i++) {
+            // if not a letter
+            if(!(isalpha(tolower(input[i])))) {
+                // if not a number
+                if(!(isdigit(input[i]))) {
+                    syntaxErr = true; 
+                } else {
+                    syntaxErr = false;
+                }
+            }
         }
-    }while( input.size() != RGB_HEX_LENGTH );
+        if( input.size() != RGB_HEX_LENGTH || input[0] != '#' || syntaxErr == true){
+            cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+        } else {
+            error = false;
+        }
+    } while( error == true);
 
     cout << "Your hex color is: " << input << endl;
 
