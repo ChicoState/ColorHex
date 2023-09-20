8810dff4676f5d9317b93147db98d95ca20b3692
Luke <130188542+lukehaz@users.noreply.github.com>
2023-09-20 11:58:57 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(){
 
     do{
         std::cout << "Enter a color in hex format (#RRGGBB):";
-        std::getline(cin, input);
+        std::getline(std::cin, input);
 
         for(int i = 1; i < input.size(); i++) {
             if(!(isalpha(tolower(input[i]))) || input[i] == 'G' || input[i] == 'H' || input[i] == 'I' || input[i] == 'J' || input[i] == 'K' || input[i] == 'L' || input[i] == 'M' || input[i] == 'N' || input[i] == 'O' || input[i] == 'P' || input[i] == 'Q' || input[i] == 'R' || input[i] == 'S' ) {      
