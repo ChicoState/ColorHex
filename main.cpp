33f4e4be25335853d9e7ad0be9b222169ebbaf8e
mbridgnell <119463264+mbridgnell@users.noreply.github.com>
2023-09-20 11:35:51 -0700

    Update main.cpp
    Added two extra tests

--- main.cpp
+++ main.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
+#include <ctype.h>
+
+using namespace std;
 
 const int RGB_HEX_LENGTH = 7;
 
 int main(){
     std::string input;
+    bool test1 = false;
+    bool test2 = false;
 
     do{
         std::cout << "Enter a color in hex format (#RRGGBB):";
         std::getline(std::cin, input);
-
-        if( input.size() != RGB_HEX_LENGTH ){
+        if (input.at(0) == '#')
+            test1 = true;
+        for (int i = 1; i < input.size(); i++)
+        {
+            if (isdigit(input.at(i)))
+            {
+                test2 = true;
+            }
+            else
+            {
+                test2 = false;
+                break;
+            }
+        }
+        if( input.size() != RGB_HEX_LENGTH || !test1 || !test2){
             std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
-    }while( input.size() != RGB_HEX_LENGTH );
+    }while( input.size() != RGB_HEX_LENGTH || !test1 || !test2);
 
     std::cout << "Your hex color is: " << input << std::endl;
 
