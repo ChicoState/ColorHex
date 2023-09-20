6fc25c0dde2a4384263959cf31aa312871eebe8d
PotatoBone <amdavid@mail.csuchico.edu>
2023-09-20 11:06:54 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
 
+using std::string;
+using std::cout;
+using std::getline;
+using std::endl;
+using std::cin;
+
 const int RGB_HEX_LENGTH = 7;
 
 int main(){
-    std::string input;
+    string input;
 
     do{
-        std::cout << "Enter a color in hex format (#RRGGBB):";
-        std::getline(std::cin, input);
-	bool invalid_char_found = false;
+        cout << "Enter a color in hex format (#RRGGBB):";
+        getline(cin, input);
+	      bool invalid_char_found = false;
         if(input.size() != RGB_HEX_LENGTH || input[0] != '#'){
-            std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+            cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
 	    input = "";
-        }
-	else{
-	    for (int i=1; i < RGB_HEX_LENGTH; i++) {
+	}
+      	else{
+	          for (int i=1; i < RGB_HEX_LENGTH; i++) {
                if ((input[i] >= '0' && input[i] <= '9') || (input[i] >= 'a' && input[i] <= 'f') || (input[i] >= 'A' && input[i] <= 'F')){
-		       invalid_char_found = false;
-	       }
-	       else{
-		       invalid_char_found = true;
-	       }
-	    }
-	}
-	if (invalid_char_found){
-            std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
-	    input = "";
-	}
+		              invalid_char_found = false;
+	             }
+	             else{
+		              invalid_char_found = true;
+                  break;
+	             }
+            }
+          	if (invalid_char_found){
+               cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+	             input = "";
+            }
+        }
     }while( input.size() != RGB_HEX_LENGTH );
 
-    std::cout << "Your hex color is: " << input << std::endl;
+    cout << "Your hex color is: " << input << endl;
 
     return 0;
 }
