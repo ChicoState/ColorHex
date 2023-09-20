441fc09c3d9f7f1130d13eea0797c39e90387952
Luke <130188542+lukehaz@users.noreply.github.com>
2023-09-20 11:31:48 -0700

    fixed ChicoState#1 ,added check that each character is good

--- main.cpp
+++ main.cpp
@@ -4,12 +4,23 @@ const int RGB_HEX_LENGTH = 7;
 
 int main(){
     std::string input;
+    bool error;
 
     do{
         std::cout << "Enter a color in hex format (#RRGGBB):";
         std::getline(cin, input);
 
-        if( input.size() != RGB_HEX_LENGTH ){
+        for(int i = 1; i < input.size(); i++) {
+            if(!(isalpha(tolower(input[i]))) || input[i] == G || input[i] == H || input[i] == I || input[i] == J || input[i] == K || input[i] == L || input[i] == M || input[i] == N || input[i] == O || input[i] == P || input[i] == Q || input[i] == R || input[i] == S ) {      
+                if(!(isdigit(input[i]))) {
+                    error = false; 
+                } else {
+                    error = true;
+                }
+            }
+        }
+
+        if( input.size() != RGB_HEX_LENGTH || input[0] != '#' || !error){
             std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
     }while( input.size() != RGB_HEX_LENGTH );
