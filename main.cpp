78d4ea0dc1fa321f156eeee65f6e23c1bf24bfb9
MichaelBrattonChico <mbratton@csuchico.edu>
2023-09-20 11:27:02 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,10 +15,29 @@ int main(){
         cout << "Enter a color in hex format (#RRGGBB):";
         getline(cin, input);
 
+		bool valid = true;
         if( input.size() != RGB_HEX_LENGTH ){
             cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
-        }
-    }while( input.size() != RGB_HEX_LENGTH );
+			valid = false;
+		}	
+		
+		if valid = true{
+		if(input[0] != "#"){
+			cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";	
+			valid = false;
+		}
+		}
+		
+		
+		if(valid = true){
+		for(int i = 0; i < input.size(); i++){
+			if(input[i] >= 'a' && input[i] <= 'f') || (input[i] >= 'A' && input[i] <= 'F' || input[i] >= '0' || input[i] <= '9'){
+				valid = true;
+			}
+		}
+		}
+		
+    }while( valid = false; );
 
     cout << "Your hex color is: " << input << endl;
 
