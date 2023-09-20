3167dcaa4ff4b565c261c394df2c89faa4b11b86
tesss24 <102249475+tesss24@users.noreply.github.com>
2023-09-20 11:36:49 -0700

    Add Hex Characters check to main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 
 const int RGB_HEX_LENGTH = 7;
 
+bool checkInput(string input){
+        for (int i = 0; i < input.size(); i++){
+                if (!isalpha(input[i]) || !isdigit(input[i]))
+                {
+                        return false;
+                }
+        }
+        return true;
+}
+
 int main(){
     std::string input;
 
@@ -9,10 +19,10 @@ int main(){
         std::cout << "Enter a color in hex format (#RRGGBB):";
         std::getline(cin, input);
 
-        if( input.size() != RGB_HEX_LENGTH ){
+        if( input.size() != RGB_HEX_LENGTH && input[0] != '#' && !checkInput(input) ){
             std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
-    }while( input.size() != RGB_HEX_LENGTH );
+    }while( input.size() != RGB_HEX_LENGTH && input[0] != '#' && !checkInput(input) );
 
     std::cout << "Your hex color is: " << input << std::endl;
 
