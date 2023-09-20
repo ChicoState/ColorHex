d7b6b7632669f1d02a6f5e277f1d06dbc47fffca
eavadney <eavadney@mail.csuchico.edu>
2023-09-20 11:34:14 -0700

    working on issue 1

--- main.cpp
+++ main.cpp
@@ -4,15 +4,25 @@ const int RGB_HEX_LENGTH = 7;
 
 int main(){
     std::string input;
+    bool alnum;
 
     do{
         std::cout << "Enter a color in hex format (#RRGGBB):";
-        std::getline(cin, input);
+        std::getline(std::cin, input);
 
-        if( input.size() != RGB_HEX_LENGTH ){
+        if((input.size() != RGB_HEX_LENGTH) || (input[0] != '#')){
             std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
-    }while( input.size() != RGB_HEX_LENGTH );
+	int x = 1;
+	alnum = true;
+	while(x < input.size()){
+		if (!isalnum(input[x])){
+			alnum = false;
+			std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+			break;
+		}
+	}
+    }while( (input.size() != RGB_HEX_LENGTH) || (input[0] != '#') || (alnum == false));
 
     std::cout << "Your hex color is: " << input << std::endl;
 
