1be202c343cc9bd1e0ab19d378c78733aadbeaaf
nscressy <nscressy@csuchico.edu>
2023-09-20 11:30:43 -0700

    Added checks for input string format closes #1

--- main.cpp
+++ main.cpp
@@ -8,11 +8,16 @@ int main(){
     do{
         std::cout << "Enter a color in hex format (#RRGGBB):";
         std::getline(cin, input);
-
-        if( input.size() != RGB_HEX_LENGTH ){
+	int wrong = 0;
+        if( input.size() != RGB_HEX_LENGTH || input[0] != '#'){
             std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
-    }while( input.size() != RGB_HEX_LENGTH );
+   	for(int i = 1; i < 7; i++)
+	{
+		if(!isdigit(input[i] || input[i] != 'a' ||input[i] != 'b' || input[i] != 'c' || input[i] != 'd' || input[i] != 'e' || input[i] != 'f')
+		{wrong = 1; std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+} 
+	}while( input.size() != RGB_HEX_LENGTH || input[0] != '#' || wrong == 1);
 
     std::cout << "Your hex color is: " << input << std::endl;
 
