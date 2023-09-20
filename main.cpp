628bfe04d3b738c0ad3e927d33516e037b66886f
Bryan Payne <bkpayne@csuchico.edu>
2023-09-20 11:38:50 -0700

    Check that each character is appropriate

--- main.cpp
+++ main.cpp
@@ -8,11 +8,24 @@ int main(){
     do{
         std::cout << "Enter a color in hex format (#RRGGBB):";
         std::getline(cin, input);
-
+	if(input[0] != '#'){
+		std::cout<< "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+		break;
+	}
+	for(int i = 0; i < input.size(); i++){
+		if(input[i] != 'A' & input[i] != 'B' & input[i] != 'C' & input[i] != 'D' & input[i] != 'E' & input[i] != 'F' & input[i] >= 0){
+			continue;
+		}
+		else{
+			std::cout<< "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+			break;
+		}
+	}
         if( input.size() != RGB_HEX_LENGTH ){
             std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
-    }while( input.size() != RGB_HEX_LENGTH );
+	
+    }while( input.size() != RGB_HEX_LENGTH | input[0] != '#');
 
     std::cout << "Your hex color is: " << input << std::endl;
 
