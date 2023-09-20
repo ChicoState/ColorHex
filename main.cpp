67d0833cf544370e802507c77cb69be234587634
Luke <130188542+lukehaz@users.noreply.github.com>
2023-09-20 11:40:07 -0700

    Update main.cpp with correct checks

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(){
         std::getline(cin, input);
 
         for(int i = 1; i < input.size(); i++) {
-            if(!(isalpha(tolower(input[i]))) || input[i] == G || input[i] == H || input[i] == I || input[i] == J || input[i] == K || input[i] == L || input[i] == M || input[i] == N || input[i] == O || input[i] == P || input[i] == Q || input[i] == R || input[i] == S ) {      
+            if(!(isalpha(tolower(input[i]))) || input[i] == 'G' || input[i] == 'H' || input[i] == 'I' || input[i] == 'J' || input[i] == 'K' || input[i] == 'L' || input[i] == 'M' || input[i] == 'N' || input[i] == 'O' || input[i] == 'P' || input[i] == 'Q' || input[i] == 'R' || input[i] == 'S' ) {      
                 if(!(isdigit(input[i]))) {
                     error = false; 
                 } else {
