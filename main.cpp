2f59bc394f28865e0f5e39fe38aee829177c678b
JedJordan <shikirawr@gmail.com>
2023-09-20 11:34:06 -0700

    Adding restrictions for input resolves #1

--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@ int main(){
 
     do{
         std::cout << "Enter a color in hex format (#RRGGBB):";
-        std::getline(cin, input);
+        std::getline(std::cin, input);
 
         if( input.size() != RGB_HEX_LENGTH ){
-            std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+            if(input[0]!= '#'){
+                std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+            }
         }
     }while( input.size() != RGB_HEX_LENGTH );
 
