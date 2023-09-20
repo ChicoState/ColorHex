092a2866266d63d35af5955d44eff07788e5ce8f
Cy <cbaca1@csuchico.edu>
2023-09-20 11:20:25 -0700

    fixes #1

--- main.cpp
+++ main.cpp
@@ -2,17 +2,22 @@
 
 const int RGB_HEX_LENGTH = 7;
 
+bool is_hex_notation(std::string const& s){
+  return s.compare(0, 1, "#") == 0
+      && s.size() == RGB_HEX_LENGTH
+      && s.find_first_not_of("0123456789abcdefABCDEF", 1) == std::string::npos;
+}
+
 int main(){
     std::string input;
 
     do{
         std::cout << "Enter a color in hex format (#RRGGBB):";
-        std::getline(cin, input);
+        std::getline(std::cin, input);
 
-        if( input.size() != RGB_HEX_LENGTH ){
+        if (!is_hex_notation(input))
             std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
-        }
-    }while( input.size() != RGB_HEX_LENGTH );
+    } while (!is_hex_notation(input));
 
     std::cout << "Your hex color is: " << input << std::endl;
 
