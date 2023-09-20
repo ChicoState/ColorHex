2883f5871a014dd8addace6d569e01bde149d3a0
tyrodgers18 <tfrodgers@csuchico.edu>
2023-09-20 11:39:23 -0700

    Fixed #1

--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 const int RGB_HEX_LENGTH = 7;
 
 int main(){
-    std::string input;
+    string input;
+    bool result = false;
 
     do{
-        std::cout << "Enter a color in hex format (#RRGGBB):";
-        std::getline(cin, input);
+        cout << "Enter a color in hex format (#RRGGBB):";
+        getline(cin, input);
 
-        if( input.size() != RGB_HEX_LENGTH ){
-            std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+        if( input.size() != RGB_HEX_LENGTH || input[0] != '#'){
+            cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
     }while( input.size() != RGB_HEX_LENGTH );
 
-    std::cout << "Your hex color is: " << input << std::endl;
+    for (int i = 1; i < input.size(); i++) {
+        if((input[i] >= 48 && input[i] <= 57) && (input[i] >= 97 && input[i] <= 102) && (input[i] >= 65 && input[i] <= 70)) {
+            result = true;
+        }
+    }
+    if (result = true){
+        cout << "Your hex color is: " << input << endl;
+    }
 
     return 0;
 }
