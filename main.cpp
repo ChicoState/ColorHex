db0283419d8030605cfbd180943b475667512fe8
gdisalvo30 <gdisalvo30@gmail.com>
2023-09-20 11:26:34 -0700

    resolved #1

--- main.cpp
+++ main.cpp
@@ -4,15 +4,24 @@ const int RGB_HEX_LENGTH = 7;
 
 int main(){
     std::string input;
+    bool hexornot = true;
 
     do{
         std::cout << "Enter a color in hex format (#RRGGBB):";
-        std::getline(cin, input);
+        std::getline(std::cin, input);
+        hexornot = true;
+        
+        for (int i = 1; i < input.size(); i++) {
+                if (!isalpha(input[i]) & !isdigit(input[i])) {
+                    hexornot = false;
+            }
+        }
 
-        if( input.size() != RGB_HEX_LENGTH ){
+        if( input.size() != RGB_HEX_LENGTH & input[0] != '#' & hexornot != true){
+          
             std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
-    }while( input.size() != RGB_HEX_LENGTH );
+    }while( input.size() != RGB_HEX_LENGTH & input[0] != '#' & hexornot != true);
 
     std::cout << "Your hex color is: " << input << std::endl;
 
