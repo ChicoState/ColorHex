b9f3addae3e0de9b0fee660566e23bd3a357b501
nolan-net <topnolan1@gmail.com>
2023-09-20 11:33:22 -0700

    Will now check for valid input

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 const int RGB_HEX_LENGTH = 7;
 
@@ -11,6 +12,13 @@ int main(){
 
         if( input.size() != RGB_HEX_LENGTH || input[0] != '#' ){
             std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+            for (int i = 1; i < input.size(); i++ ) {
+                //Check if input is a valid hex value
+              if (input[i] > 9 || input[i] < 0 || input[i] > 'F' || input[i] < 'A') {
+                std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+                break;
+              }
+            }
         }
     }while( input.size() != RGB_HEX_LENGTH );
 
