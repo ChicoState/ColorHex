b3f8f22f0e5df255697f98bd40dc5588f778065e
khasek <kmhasek@gmail.com>
2023-09-20 11:54:09 -0700

    More progress on hex check

--- main.cpp
+++ main.cpp
@@ -10,13 +10,19 @@ using std::vector;
 
 const int RGB_HEX_LENGTH = 7;
 
-bool isValid(string hex) {
-    std::vector<string> validValues = {"0", "1", "2", "3", "4", "5", "6",
+bool isValid(string hex){
+    vector<string> validValues = {"0", "1", "2", "3", "4", "5", "6",
         "7", "8", "9", "a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F"};
 
-    if (hex[0] != "#") {
-        
+    if(hex[0] != "#"){
+        return false;
     }
+
+    for(int i; i < hex.length(); i++){
+        // return false if hex[i] is not included in the list of valid values
+    }
+
+    return true;
 }
 
 int main(){
