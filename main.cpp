4aec26067aae70183606d7fa161ba3c8a8f9f47a
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2023-09-20 11:41:15 -0700

    resolves Repeats for wrong length input & Remove namespace

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 
+using std::cout;
+using std::cin;
+using std::getline;
+using std::string;
+using std::endl;
+
 const int RGB_HEX_LENGTH = 7;
 
 int main(){
-    std::string input;
-
-    do{
+	std::string input;
+	bool hexValue = true;
+    while (hexValue) {
         std::cout << "Enter a color in hex format (#RRGGBB):";
         std::getline(cin, input);
 
-        if( input.size() != RGB_HEX_LENGTH ){
-            std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+        if( input.size() != RGB_HEX_LENGTH ){  
+	  std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
-    }while( input.size() != RGB_HEX_LENGTH );
-
-    std::cout << "Your hex color is: " << input << std::endl;
-
+    }if( input.size() == RGB_HEX_LENGTH ){
+	std::cout << "Your hex color is: " << input << std::endl;
+	hexValue = false;
+     }
     return 0;
 }
