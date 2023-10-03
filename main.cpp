de54f695dc9d6093137b58038e96798da02f1f5f
tyrodgers18 <tfrodgers@csuchico.edu>
2023-10-03 13:52:04 -0700

    Fixed #1 - Check that input is an appropriate hex value

--- main.cpp
+++ main.cpp
@@ -1,30 +1,37 @@
 #include <iostream>
 #include <string>
+#include <cctype>
+
 using namespace std;
+using std::string;
+using std::cout;
+using std::getline;
+using std::endl;
+using std::cin;
 
 const int RGB_HEX_LENGTH = 7;
 
 int main(){
     string input;
-    bool result = false;
-
+    bool allow;
     do{
+        allow = true;
         cout << "Enter a color in hex format (#RRGGBB):";
         getline(cin, input);
-
+        for (int i = 1; i < input.size(); i++) {
+            if (!isxdigit(input[i])) {
+                allow = false;
+            }
+        }
         if( input.size() != RGB_HEX_LENGTH || input[0] != '#'){
             cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
-    }while( input.size() != RGB_HEX_LENGTH );
-
-    for (int i = 1; i < input.size(); i++) {
-        if((input[i] >= 48 && input[i] <= 57) && (input[i] >= 97 && input[i] <= 102) && (input[i] >= 65 && input[i] <= 70)) {
-            result = true;
+        else if (allow == false) {
+            cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
-    }
-    if (result = true){
-        cout << "Your hex color is: " << input << endl;
-    }
+    }while( (input.size() != RGB_HEX_LENGTH) || (allow == false) || (input[0] != '#'));
+
+    cout << "Your hex color is: " << input << endl;
 
     return 0;
 }
