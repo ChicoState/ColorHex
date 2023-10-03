2d3918d9db4c0aadfe1303904e95ccd4f5e6926d
Adam Sanden <acsanden@csuchico.edu>
2023-10-03 13:49:15 -0700

    addresses input validation detailed in issue 1

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
+#include <string>
 
 const int RGB_HEX_LENGTH = 7;
-
 int main(){
     std::string input;
-
+    bool state;
+    
     do{
+        state = true;
         std::cout << "Enter a color in hex format (#RRGGBB):";
         std::getline(std::cin, input);
 
         if( input.size() != RGB_HEX_LENGTH ){
             std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+            state = false;
+        }else if(input[0] != '#' || (!isxdigit(input[1]) || !isxdigit(input[2]) || !isxdigit(input[3]) || !isxdigit(input[4]) || !isxdigit(input[5]) || !isxdigit(input[6]))){
+            std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+            state = false;
         }
-    }while( input.size() != RGB_HEX_LENGTH );
+    }while( input.size() != RGB_HEX_LENGTH || state == false );
 
     std::cout << "Your hex color is: " << input << std::endl;
 
